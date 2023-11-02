e470682bc9e35c67943f6a46f198fe8793ad2ae9
小傅哥 <184172133@qq.com>
2023-11-03 07:57:54 +0800

    小傅哥，feat：shardingjdbc 案例

---
+++ xfg-dev-tech-infrastructure/src/main/java/cn/bugstack/xfg/dev/tech/infrastructure/dao/IUserOrderDao.java
@@ -0,0 +1,17 @@
+package cn.bugstack.xfg.dev.tech.infrastructure.dao;
+
+import cn.bugstack.xfg.dev.tech.infrastructure.po.UserOrderPO;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface IUserOrderDao {
+
+    void insert(UserOrderPO userOrderPO);
+
+    void updateOrderStatusByUserId(String userId);
+
+    List<UserOrderPO> selectByUserId(String userId);
+
+}
